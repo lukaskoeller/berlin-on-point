6c9365d0b997014c0efb2dda7e92806e7f78a309
lukaskoeller <lukas-koeller@onlinehome.de>
2020-12-06 14:33:39 +0100

    feat(backend): receive form data via POST /subscribe w/ multer

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const cors = require('cors');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 const services = require('./services');
 
 const express = require('express');
@@ -6,10 +8,16 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
 
 app.get('/services', (req, res) => {
     console.log('Send all services');
     res.json(services);
+});
+
+app.post('/subscribe', upload.none(), (req, res) => {
+    console.log("req.body", req.body);
 });
 
 app.listen(port, () => {
