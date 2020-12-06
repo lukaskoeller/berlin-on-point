a4433415867e79eb84163d73339ce96c4e6da83f
lukaskoeller <lukas-koeller@onlinehome.de>
2020-12-06 10:19:06 +0100

    chore(/services): move services to folder

---
+++ backend/services/index.js
@@ -0,0 +1,28 @@
+/**
+ * Listed services of Berlin authorities
+ * See https://service.berlin.de/dienstleistungen/
+ */
+const services = {
+    registrationOfApartment: {
+      no: '120686',
+      name: 'Anmeldung einer Wohnung',
+    },
+    applyForID: {
+      no: '120703',
+      name: 'Personalausweis beantragen',
+    },
+    applyForPassport: {
+      no: '121151',
+      name: 'Reisepass beantragen',
+    },
+    severelyHandicappedPass: {
+      no: '121737',
+      name: 'Schwerbehindertenausweis',
+    },
+    certificateOfGoodConduct: {
+      no: '120926',
+      name: 'Führungszeugnis',
+    },
+  };
+
+  module.exports = services;
