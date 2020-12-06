eb3f026dfa8562c5c31f6b62b01745732ec8c505
lukaskoeller <lukas-koeller@onlinehome.de>
2020-12-06 10:17:44 +0100

    chore(backend/index): first implementation of express app

--- backend/index.js
+++ backend/index.js
@@ -1,77 +1,19 @@
-/* eslint-disable no-await-in-loop */
-const puppeteer = require('puppeteer');
+const cors = require('cors');
+const services = require('./services');
 
-const baseURL = 'https://service.berlin.de';
-const servicePath = 'dienstleistung';
-const lookupMonths = 6; // Amount of months (from present to future) to check for.
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const services = {
-  registrationOfApartment: '120686',
-};
+app.use(cors());
 
-/**
- * getFreeSlots - Function that looks for free slots to book an appointment
- * for a service of Berlin's authorities.
- * @param {string} service Name of service e.g. "Anmeldung einer Wohnung".
- */
-const getFreeSlots = async (service) => {
-  let availability = []; // All available dates with booking links for a given service.
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+app.get('/services', (req, res) => {
+    console.log('Send all services');
+    res.json(services);
+});
 
-  /**
-   * @returns {object} the available date as ISO String and the accoding link to book it.
-   */
-  const getAvailableDays = async () => {
-    availability = [
-      ...availability,
-      ...await page.$$eval(
-        '.calendar-table td[class="buchbar"] > a',
-        (days) => days.map((day) => ({
-          date: new Date(parseInt(
-            day.href.match(/(?<=\/)\d+(?=\/)/)[0] * 1000, // Get numeric UTC in seconds.
-            10,
-          )).toISOString(),
-          link: day.href,
-        })),
-      ),
-    ];
-  };
+app.listen(port, () => {
+    console.log(`berlin on point listening at http://localhost:${port}`);
+});
 
-  // Go to Service Page
-  await page.goto(`${baseURL}/${servicePath}/${service}`);
-  await page.waitForSelector('.zmstermin-multi.inner');
-  // await page.screenshot({ path: 'screenshot.png' });
-
-  // Go to Appointments Overview Page
-  const bookAppointment = await page.$eval('.zmstermin-multi.inner > a[href]', (a) => a.href);
-  await page.goto(bookAppointment);
-  await page.waitForSelector('.calendar-table');
-
-  for (let index = 1; index <= lookupMonths / 2; index += 1) {
-    // Get availability for given two months.
-    await getAvailableDays();
-
-    // Go to overview for following two months.
-    try { await page.click('th.next > a'); } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(`Error: Only can look up ${index * 2} months.`);
-      break;
-    }
-    await page.waitForSelector('.calendar-table');
-
-    try { await page.click('th.next > a'); } catch (error) {
-      await getAvailableDays();
-      // eslint-disable-next-line no-console
-      console.error(`Error: Only can look up ${index * 2 - 1} months.`);
-      break;
-    }
-    await page.waitForSelector('.calendar-table');
-    // eslint-disable-next-line no-console
-    console.log(index, ': ', availability);
-  }
-
-  // await browser.close();
-};
-
-getFreeSlots(services.registrationOfApartment);
+// module.exports = app;
